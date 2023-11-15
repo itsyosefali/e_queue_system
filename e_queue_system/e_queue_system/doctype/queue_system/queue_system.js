60bd824cfd7e81c2c4db5008df1753b6ca06d1dd
itsyosefali <joeyxjoey123@gmail.com>
2023-11-16 00:18:25 +0200

    first commit for queue system

---
+++ e_queue_system/e_queue_system/doctype/queue_system/queue_system.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, itsyosefali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Queue System', {
+	// refresh: function(frm) {
+
+	// }
+});
